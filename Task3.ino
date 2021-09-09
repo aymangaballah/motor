4eae81f63fd700634b2461c24591b39a6c5abb7d
aymangaballah <aymangaballah71@gmail.com>
2021-09-09 20:10:18 +0200

    led code

---
+++ Task3.ino
@@ -0,0 +1,21 @@
+#define LED_1 13
+#define LED_2 12
+#define LED_3 11
+void setup()
+{
+  pinMode(LED_1, OUTPUT);
+  pinMode(LED_2, OUTPUT);
+  pinMode(LED_3, OUTPUT);
+}
+ 
+void loop()
+{
+  digitalWrite(LED_1, HIGH);
+  digitalWrite(LED_2, HIGH);
+  digitalWrite(LED_3, HIGH);
+  
+  
+ 
+ 
+ 
+}
